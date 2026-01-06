fca6072bbdcc7c6128e0e4c44ba16687545e6445
Daksha-Belawadi <belawadidaksha@gmail.com>
2026-01-06 21:42:10 +0530

    save

---
+++ bank/bank1.py
@@ -0,0 +1,7 @@
+def bank_details(account_no, holder_name, account_type, balance):
+    return (
+        f"Account Number : {account_no}\n"
+        f"Account Holder Name : {holder_name}\n"
+        f"Account Type : {account_type}\n"
+        f"Balance : {balance}"
+    )
